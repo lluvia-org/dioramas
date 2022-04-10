a2f64e89680631d75585181a9ece031f3966abc3
Vincent Smuda <vincent.smuda@lluvia.studio>
2022-04-10 15:21:48 -0600

    Adds eslint

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+	'env': {
+		'browser': true,
+		'es2021': true
+	},
+	'extends': 'eslint:recommended',
+	'parserOptions': {
+		'ecmaVersion': 'latest',
+		'sourceType': 'module'
+	},
+	'globals': {
+		'__dirname': 'readonly',
+		'require': 'readonly',
+		'module': 'writable',
+	},
+	'rules': {
+		'indent': [
+			'error',
+			'tab'
+		],
+		'linebreak-style': [
+			'error',
+			'unix'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'always'
+		]
+	}
+};
